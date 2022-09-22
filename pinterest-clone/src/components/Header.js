ea716fd3755e92bd1b2bd91a21ccd9f0d20d50e6
Therese Tshimanga <theresetshimanga@TT.local>
2022-09-23 01:14:22 +0200

    v2

--- pinterest-clone/src/components/Header.js
+++ pinterest-clone/src/components/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 import PinterestIcon from '@mui/icons-material/Pinterest';
 // import { CenterFocusStrong } from '@mui/icons-material';
 // import { height, padding, style } from '@mui/system';
 import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/material/SearchIcon';
+import NotificationsIcon from '@mui/material/NotificationsIcon';
+import FaceIcon from '@mui/material/FaceIcon';
+import TextSmsIcon from '@mui/material/TextsmsIcon';
+import KeyboardArrowDownIcon from '@mui/material/KeyboardArrowDownIcon';
+
 
 
 
@@ -19,24 +25,26 @@ function Header() {
                   <PinterestIcon />
               </IconButton>
           </LogoWrapper>
-
           <HomePageButton>
               <a href="/">Homepage</a>
           </HomePageButton>
-          {/* <Followingbutton>
+          <FollowingButton>
             <a href="/">Following</a>
-          </Followingbutton> */}
-          
-          {/* <SearchWrapper>
+          </FollowingButton>
+           <SearchWrapper>
               <SearchBarWrapper>
-
               </SearchBarWrapper>
-
+              <IconButton>
+                  <SearchIcon />
+              </IconButton>
+              <form>
+                  <input type = 'text' />
+                  <button type = 'submit'></button>
+              </form>
               </SearchWrapper>
               <Iconwrapperwrapper>
-
-                </Iconwrapperwrapper> */} 
-      </Wrapper> 
+                </Iconwrapperwrapper> 
+      </Wrapper>  
      
       </div>
    
@@ -65,14 +73,17 @@ const LogoWrapper = styled.div`
  }
 
 `
-const HomePageButton = styled.div`
+
+const HomeButtons = styled.div`
 display: flex;
-height: 48px;
-min-width:123px;
+height:48px;
+min-width: 123px;
 align-items: center;
 justify-content: center;
 border-radius:24px;
-cursor: pointer;
+`
+
+const HomePageButton = styled(HomeButtons)`
 background-color: rgb(17, 17, 17);
 
 a{
@@ -80,5 +91,46 @@ a{
     color:white;
     font-weight: 700;
 }
+`
 
+const FollowingButton = styled(HomeButtons)`
+background-color: white;
+
+a{
+    text-decoration:none
+    color: black;
+    font-weight: 700;
+}
+
+;hover{
+    background-color: #e1e1e1;
+}
+`
+const SearchWrapper = styled.div`
+    flex: 1;
+`
+const SearchBarWrapper = styled.div`
+    background-color: #efefef;
+    display: flex;
+    height: 48px;
+    width: 100%;
+    border-radius: 50px;
+    border: none;
+    padding-left: 10px;
+
+    form > input {
+        background-color: transparent;
+        border: none;
+        width: 100%;
+        margin-left: 5px;
+        font-size: 16px;
+    }
+
+    form > button {
+        display: none;
+    }
+
+    input:focus {
+        outline: none;
+    }
 `
